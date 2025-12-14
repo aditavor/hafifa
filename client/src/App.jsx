185e26a4584a2e04c41e25eb6bdab579957f01de
user <aditavor8@gmail.com>
2025-12-14 12:50:07 +0200

    feat(App.jsx):add page paths

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home.jsx";
+import Login from "./Pages/Login/Login.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
