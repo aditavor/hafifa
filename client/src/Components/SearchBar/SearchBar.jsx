ee9d55ae716c8a27eec131c2fa0ab331d316b154
user <aditavor8@gmail.com>
2025-12-14 11:50:22 +0200

    feat(Book):create single book component

--- client/src/Components/SearchBar/SearchBar.jsx
+++ client/src/Components/SearchBar/SearchBar.jsx
@@ -1,22 +1,14 @@
-import { useState } from "react";
 import "./SearchBar.scss";
 
-function SearchBar({ placeholder = "Search...", onSearch }) {
-  const [value, setValue] = useState("");
-
-  const handleChange = (e) => {
-    setValue(e.target.value);
-    if (onSearch) onSearch(e.target.value);
-  };
-
+function SearchBar({ value, onChange }) {
   return (
     <div className="searchbar">
       <i className="searchIcon fa-solid fa-magnifying-glass"></i>
       <input
         type="text"
+        placeholder="Search..."
         value={value}
-        placeholder={placeholder}
-        onChange={handleChange}
+        onChange={(e) => onChange(e.target.value)}
       />
     </div>
   );
