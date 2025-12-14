177e4b9fed3ce4e65ee58b94a8255c87350f19b5
user <aditavor8@gmail.com>
2025-12-14 12:30:29 +0200

    feat(App.js):change import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home.jsx";
-import Login from "./Pages/Login/Login.jsx";
-import Register from "./Pages/Register.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
