beef7a8eb54c0cb06a973cb12aaf5b5c454d5858
user <aditavor8@gmail.com>
2025-12-16 13:43:39 +0200

    feat(customers):make page for customers details

---
+++ client/src/Pages/Customers/Customers.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import Card from "../../Components/Card/Card";
+import "./Customers.scss";
+import Collapse from "../../Components/Collaspe/Collapse";
+import LibUsers from "../../Components/LibUsers/LibUsers";
+
+function Customers() {
+  const [customers, setCustomers] = useState([]);
+  const [timeoutCustomer, setTimeoutCustomer] = useState([]);
+
+  const fetchCustomers = async () => {
+    let res = await fetch("http://localhost:3000/users");
+    let data = await res.json();
+    setCustomers(data);
+
+    res = await fetch("http://localhost:3000/users/returnTimeout");
+    data = await res.json();
+    setTimeoutCustomer(data);
+  };
+
+  useEffect(() => {
+    fetchCustomers();
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <div className="page-container">
+        <div className="container">
+          <Collapse title="Customers">
+            <LibUsers users={customers} errorMessage={"no users"}></LibUsers>
+          </Collapse>
+        </div>
+
+        <div className="container">
+          <Collapse title="Customers in late">
+            <LibUsers
+              users={timeoutCustomer}
+              errorMessage={"no users in late"}
+            ></LibUsers>
+          </Collapse>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Customers;
