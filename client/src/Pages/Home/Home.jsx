65908559e4a46933197044b3ef873fe152b320d4
user <aditavor8@gmail.com>
2025-12-17 14:13:55 +0200

    feat(Home):likned button to library book from home

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,11 +1,18 @@
 import Navbar from "../../Components/Navbar/Navbar";
+import "./Home.scss";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
+
   return (
     <>
       <Navbar />
       <div className="page-container">
         <h2 className="title">Welcome to the library</h2>
+        <button className="to-library" onClick={() => navigate("/library")}>
+          View library's books
+        </button>
       </div>
     </>
   );
