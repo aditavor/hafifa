fb460a074e22cff1e04bdf436f1e0b2e1768f2c5
user <aditavor8@gmail.com>
2025-12-17 13:15:08 +0200

    feat(App,personal):add page to view and handle your books

---
+++ client/src/Pages/Personal/Personal.jsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import Card from "../../Components/Card/Card";
+
+function Personal() {
+  const [books, setBooks] = useState([]);
+  const userId = localStorage.getItem("user_id");
+
+  const fetchBooks = async () => {
+    let res = await fetch("http://localhost:3000/users/" + userId + "/books");
+    let data = await res.json();
+    setBooks(data);
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const handleBorrow = async (bookId, bookName) => {
+    try {
+      const res = await fetch(
+        "http://localhost:3000/books/" + bookId + "/return",
+        {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+
+      if (!res.ok) {
+        console.error("Failed to return book");
+        return;
+      }
+
+      console.log(`Returned: ${bookName}`);
+    } catch (err) {
+      console.error(err);
+    }
+
+    fetchBooks();
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="page-container">
+        <h2 className="title">Books you borrowed:</h2>
+        <div className="container">
+          <div className="container-item">
+            {books.length > 0 ? (
+              books.map((book) => (
+                <Card
+                  key={book.id}
+                  data={{
+                    id: book.id,
+                    name: book.name,
+                    headers: "Borrow Date: " + book.borrow_date,
+                  }}
+                  btnData={"Return Book"}
+                  onBorrow={handleBorrow}
+                  showIcon={false}
+                  showBtn={true}
+                />
+              ))
+            ) : (
+              <p>No Books in library</p>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Personal;
