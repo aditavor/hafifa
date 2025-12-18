773e78631130db1bd48405e300a5c210485a6c69
user <aditavor8@gmail.com>
2025-12-18 16:07:51 +0200

    feat(Home):add list of mmost popular books

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,18 +1,45 @@
+import { useEffect, useState } from "react";
 import Navbar from "../../Components/Navbar/Navbar";
 import "./Home.scss";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
   const navigate = useNavigate();
+  const [books, setBooks] = useState([]);
+
+  const fetchBooks = async () => {
+    const res = await fetch("http://localhost:3000/books/mostPopular");
+    const data = await res.json();
+    setBooks(data);
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
 
   return (
     <>
       <Navbar />
       <div className="page-container">
-        <h2 className="title">Welcome to the library</h2>
+        <h1 className="title">Welcome to the library</h1>
         <button className="to-library" onClick={() => navigate("/library")}>
           View library's books
         </button>
+        <h3 className="title">Our Top 10 Bestsellers:</h3>
+        <ul className="list">
+          {books.length !== 0 ? (
+            books.map((book) => (
+              <li key={book.id} className="list-item">
+                <div>
+                  <h3>{book.name}</h3>
+                  <p>Borrows: {book.borrows}</p>
+                </div>
+              </li>
+            ))
+          ) : (
+            <p>No Books</p>
+          )}
+        </ul>
       </div>
     </>
   );
