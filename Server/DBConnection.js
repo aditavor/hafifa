574e409c917b8632da206879fdbe726b66e0f170
user <aditavor8@gmail.com>
2025-12-24 16:38:03 +0200

    feat(server):protected DB connection data

--- Server/DBConnection.js
+++ Server/DBConnection.js
@@ -1,9 +1,11 @@
 const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const sequelize = new Sequelize("postgres", "postgres", "adi.tavor2006", {
-  host: "localhost",
-  dialect: "postgres",
-  port: 5432,
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  port: Number(process.env.DB_PORT),
   logging: false,
 });
 
